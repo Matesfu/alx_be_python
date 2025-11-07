33c24b0cecda770c04d00490fea5236f698ad7ca
Matesfu <117965680+Matesfu@users.noreply.github.com>
2025-11-07 16:56:03 +0300

    Fix variable names for monthly expenses and savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = float(input("Enter your monthly income: "))
-monthly_expense = float(input("Enter your total monthly expenses: "))
-monthly_saving = monthly_income - monthly_expense
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 annual_interest = 0.05
-projected_saving = (monthly_saving * 12) + (monthly_saving * 12 * annual_interest)
-print(f"Your monthly savings are ${monthly_saving}.")
+projected_saving = (monthly_savings * 12) + (monthly_savings * 12 * annual_interest)
+print(f"Your monthly savings are ${monthly_savings}.")
 print(f"Projected savings after one year, with interest, is: ${projected_saving}.")
