341ab8b7ad4d64d8f45cbce723ee859385b9d654
Tesfahun Kassahun <tesfu27kassahun@gmail.com>
2025-11-21 13:23:35 +0300

    shopping_list_manager.py edied

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item_append = input("Enter the item name: ")
+            item_append = input("Enter the item to add: ")
             shopping_list.append(item_append)
         elif choice == '2':
             # Prompt for and remove an item
