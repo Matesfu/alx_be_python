c06d5294eacaff76a39fd3ce5fc833476ec4c322
Tesfahun Kassahun <tesfu27kassahun@gmail.com>
2025-11-28 21:59:53 +0300

    bank_account edited

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -11,6 +11,6 @@ class BankAccount:
         else:
             return False
     def display_balance(self):
-        print("your account balance is: ", self.account_balance)
+        print("Current Balance:", self.account_balance)
     
 
